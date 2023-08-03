54c1c3ff655b05891e63ba4b0223ebc646c16086
rudeboytae <wjdxosud@gmail.com>
2023-08-03 17:39:52 +0900

    정태녕 radio, 추천페이지 추가

--- travelmaker/src/App.js
+++ travelmaker/src/App.js
@@ -11,6 +11,7 @@ import './components/css/Myschedule.css';
 import './components/css/Recommend.css';
 import './components/css/Information.css';
 import './components/css/Application.css';
+import './components/css/Scheduledetail.css';
 
 // 컴포넌트 불러오기
 import Header from './components/Header';
@@ -24,6 +25,7 @@ import Membership from './components/Membership';
 import Myschedule from './components/Myschedule';
 import Information from './components/Information';
 import Application from './components/Application'
+import Scheduledetail from './components/Scheduledetail';
 
 
 import { Routes, Route } from 'react-router-dom';
@@ -43,6 +45,7 @@ function App() {
         <Route path='/myschedule' element={<Myschedule />}></Route>
         <Route path='/information' element={<Information />}></Route>
         <Route path='/application' element={<Application />}></Route>
+        <Route path='/scheduledetail' element={<Scheduledetail />}></Route>
       </Routes>
     </div>
   );
--- travelmaker/src/components/Recommend.jsx
+++ travelmaker/src/components/Recommend.jsx
@@ -1,38 +1,40 @@
 import React from 'react'
 
 const Recommend = () => {
-    return (
-      <div className='recommend-container'>
-        <nav className='recommend-nav-list'>
-        </nav>
-  
-        <div className='recommend-box'>
-          <div className='recommend-application'>
-            <div className='recommend-select'>
-              <div className='recommend-select-all'>전체보기</div>
-              <div className='recommend-select-reco'>추천</div>
-            </div>
-          </div>
+  return (
+    <div className='recommend-container'>
+      <nav className='recommend-nav-list'>
+      </nav>
 
-          <div className='recommend-application'>
-            <div className='recommend-select'>
-              <div className='reommend-select-location'>지역 선택</div>
-              <div className='recommend-select-day'><input type='date'></input></div>
-            </div>
+      <div className='recommend-box'>
+        <div className='recommend-application'>
+          <div className='recommend-select'>
+            <input id='my_t' type="radio" name="my_sch" />
+            <label className='my_txt' for='my_t' >내가 작성한 글</label>
+            <input id='my_s' type="radio" name="my_sch" />
+            <label className='my_txt' for='my_s'>신청한 사람 목록</label>
           </div>
-  
-          <div className='recommend-form'>
-            <div className='recommend-list'>
-              <div>일정</div>
-              <div>작성자</div>
-              <div>내용</div>
-              <div>내용</div>
-              <div>내용</div>
-            </div>
+        </div>
+
+        <div className='recommend-application'>
+          <div className='recommend-select'>
+            <div className='reommend-select-location'>지역 선택</div>
+            <div className='recommend-select-day'><input type='date'></input></div>
+          </div>
+        </div>
+
+        <div className='recommend-form'>
+          <div className='recommend-list'>
+            <div>일정</div>
+            <div>작성자</div>
+            <div>내용</div>
+            <div>내용</div>
+            <div>내용</div>
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Recommend
