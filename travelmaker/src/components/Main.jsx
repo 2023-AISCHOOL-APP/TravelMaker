5891bb8edcab5bdd81cce49b2b71982def8e070d
rudeboytae <wjdxosud@gmail.com>
2023-08-10 15:45:16 +0900

    정태녕 오후 풀

--- travelmaker/src/components/Main.jsx
+++ travelmaker/src/components/Main.jsx
@@ -7,33 +7,37 @@ const Main = () => {
   const nav = useNavigate();
 
   // 로그인 상태에 따라 접근권한 부여
-  const search = ()=>{
-    if(userID != null){
+  const search = () => {
+    if (userID != null) {
       nav('/select')
-    }else{
+    } else {
       alert('로그인 후 사용해주세요!')
       nav('/login')
     }
   }
   return (
     <div className='main-container'>
-      <div className='main-contant-box'>
-        <div>
-          <h1 className='main-title'>Travel Mate</h1>
+  
+
+        <div className='main-contant-box'>
+          <div>
+            <h1 className='main-title'>Travel Mate</h1>
+          </div>
+          <div>
+            <p className='main-text'>새로운 친구와 즐거운 여행을 만들어보세요!</p>
+          </div>
+          {/* 로그인 상태에 따라 동행찾기 접근 권한 설정 */}
+          <div onClick={search}>
+            <Link>
+              <a className='main_btn click-b b' >동행찾기</a>
+            </Link>
+          </div>
         </div>
-        <div>
-          <p className='main-text'>새로운 친구와 즐거운 여행을 만들어보세요!</p>
+
+        <div className='main-img-box'>
+          <img src='images/bgimg.jpg' className='main-img'></img>
         </div>
-        {/* 로그인 상태에 따라 동행찾기 접근 권한 설정 */}
-        <div onClick={search}>
-          <Link>
-            <a className='main_btn click-b b' >동행찾기</a>
-          </Link>
-        </div> 
-      </div>
-      <div className='main-img-box'>
-        <img src='images/bgimg.jpg' className='main-img'></img>
-      </div>
+
     </div>
   )
 }
